e79f36f5a5f513fb994f23d46728a57ff4f0a1d1
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-06-08 11:01:59 +0100

    Add core functionality for user data management and testing

---
+++ script.mjs
@@ -0,0 +1,6 @@
+import { getUserIds } from "./common.mjs";
+
+window.onload = function () {
+  const users = getUserIds();
+  //document.querySelector("body").innerText = `There are ${users.length} users`;
+};
