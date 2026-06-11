f4c9b338049a83b6d9bc95a9c898969d6d310d49
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-06-11 19:39:38 +0100

    simplify date assertions in createAgendaItem tests

--- agenda.test.mjs
+++ agenda.test.mjs
@@ -7,10 +7,10 @@ test("create revision dates from a start date",()=>{
     const result=createAgendaItem(topic,startDate)
     
     assert.equal(result.length, 5);
-    assert.equal(result[0].date.toISOString().split("T")[0], "2027-07-26");
-    assert.equal(result[1].date.toISOString().split("T")[0], "2027-08-19");
-    assert.equal(result[2].date.toISOString().split("T")[0], "2027-10-19");
-    assert.equal(result[3].date.toISOString().split("T")[0], "2028-01-19");
-    assert.equal(result[4].date.toISOString().split("T")[0], "2028-07-19");
+    assert.equal(result[0].date, "2027-07-26");
+    assert.equal(result[1].date, "2027-08-19");
+    assert.equal(result[2].date, "2027-10-19");
+    assert.equal(result[3].date, "2028-01-19");
+    assert.equal(result[4].date, "2028-07-19");
     
 })
