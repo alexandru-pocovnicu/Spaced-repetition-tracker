421d6c35e6c03e8a4e79d2d167a07923d5951edb
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-06-11 15:07:38 +0100

    Add form submission and agenda item creation functionality

--- script.mjs
+++ script.mjs
@@ -1,18 +1,79 @@
 import { getUserIds } from "./common.mjs";
-import { getData } from "./storage.mjs";
+import { getData, addData } from "./storage.mjs";
 const users = getUserIds();
 let usersDropdown;
 let message;
 let agendaList;
 let topicForm;
+let topicName;
+let revisionDate;
 window.onload = function () {
   usersDropdown = document.getElementById("users-dropdown");
   message = document.getElementById("message");
   agendaList = document.getElementById("agenda-list");
   topicForm = document.getElementById("topic-form");
+  topicName = document.getElementById("topic-name");
+  revisionDate = document.getElementById("revision-date");
   addUsersToDropdown();
   usersDropdown.addEventListener("change", selectedUser);
+  topicForm.addEventListener("submit", formSubmit);
 };
+
+function formSubmit(event) {
+  event.preventDefault();
+  const selectedUserId = usersDropdown.value;
+
+  const topic = topicName.value.trim();
+  if (topic.length < 3) {
+    alert("Please enter a valid topic with a minimum of 3 characters");
+    return;
+  }
+
+  const date = revisionDate.value;
+
+  const agendaItems = createAgendaItem(topic, date);
+}
+
+function createAgendaItem(topic, startDate) {
+  const date = new Date(startDate);
+  const oneWeek = new Date(date);
+  oneWeek.setDate(oneWeek.getDate() + 7);
+
+  const oneMonth = new Date(date);
+  oneMonth.setMonth(oneMonth.getMonth() + 1);
+
+  const threeMonths = new Date(date);
+  threeMonths.setMonth(threeMonths.getMonth() + 3);
+
+  const sixMonths = new Date(date);
+  sixMonths.setMonth(sixMonths.getMonth() + 6);
+
+  const oneYear = new Date(date);
+  oneYear.setFullYear(oneYear.getFullYear() + 1);
+
+  return [
+    {
+      topic: topic,
+      date: oneWeek,
+    },
+    {
+      topic: topic,
+      date: oneMonth,
+    },
+    {
+      topic: topic,
+      date: threeMonths,
+    },
+    {
+      topic: topic,
+      date: sixMonths,
+    },
+    {
+      topic: topic,
+      date: oneYear,
+    },
+  ];
+}
 
 function addUsersToDropdown() {
   for (const user of users) {
@@ -46,7 +107,8 @@ function displayAgenda(userAgenda) {
   agendaList.innerHTML = "";
   for (const item of userAgenda) {
     const listItem = document.createElement("li");
-    listItem.innerText = item;
+    listItem.innerText = `${item.topic} - ${item.date}`;
+
     agendaList.append(listItem);
   }
 }
