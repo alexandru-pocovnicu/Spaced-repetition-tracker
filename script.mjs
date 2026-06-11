66b0938b312594e0b45eeb9e21df3414e45e4472
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-06-11 10:21:25 +0100

    Add agenda list functionality to display user-specific agendas

--- script.mjs
+++ script.mjs
@@ -3,9 +3,11 @@ import { getData } from "./storage.mjs";
 const users = getUserIds();
 let usersDropdown;
 let message;
+let agendaList
 window.onload = function () {
   usersDropdown = document.getElementById("users-dropdown");
   message = document.getElementById("message");
+  agendaList=document.getElementById("agenda-list")
   addUsersToDropdown();
   usersDropdown.addEventListener("change", selectedUser);
 };
@@ -30,5 +32,15 @@ function selectedUser() {
     message.hidden = false;
   } else {
     message.hidden = true;
+    displayAgenda(userAgenda)
+  }
+}
+
+function displayAgenda(userAgenda){
+  agendaList.innerHTML=""
+  for(const item of userAgenda){
+    const listItem=document.createElement("li")
+    listItem.innerText=item
+    agendaList.append(listItem)
   }
 }
