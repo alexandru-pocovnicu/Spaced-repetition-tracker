ff8c98994164b4ede6db58a27328a68093648398
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-06-11 08:18:50 +0100

    get selected user value

--- script.mjs
+++ script.mjs
@@ -1,15 +1,22 @@
 import { getUserIds } from "./common.mjs";
 const users = getUserIds();
+let usersDropdown;
+
 window.onload = function () {
+  usersDropdown = document.getElementById("users-dropdown");
   addUsersToDropdown();
+  usersDropdown.addEventListener("change", selectedUser);
 };
 
 function addUsersToDropdown() {
-  const usersDropdown = document.getElementById("users-dropdown");
   for (const user of users) {
     const option = document.createElement("option");
     option.innerText = `User ${user}`;
     option.value = user;
     usersDropdown.append(option);
   }
+}
+
+function selectedUser() {
+  const selectedUserId = usersDropdown.value;
 }
