48ccd41030312d83e91114fa670f4f41d205b8af
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-06-11 18:31:11 +0100

    delete redundant code

--- script.mjs
+++ script.mjs
@@ -64,46 +64,6 @@ function formSubmit(event) {
   revisionDate.value = getTodayDate();
 }
 
-//export function createAgendaItem(topic, startDate) {
-  const date = new Date(startDate);
-  const oneWeek = new Date(date);
-  oneWeek.setDate(oneWeek.getDate() + 7);
-
-  const oneMonth = new Date(date);
-  oneMonth.setMonth(oneMonth.getMonth() + 1);
-
-  const threeMonths = new Date(date);
-  threeMonths.setMonth(threeMonths.getMonth() + 3);
-
-  const sixMonths = new Date(date);
-  sixMonths.setMonth(sixMonths.getMonth() + 6);
-
-  const oneYear = new Date(date);
-  oneYear.setFullYear(oneYear.getFullYear() + 1);
-
-  return [
-    {
-      topic: topic,
-      date: oneWeek,
-    },
-    {
-      topic: topic,
-      date: oneMonth,
-    },
-    {
-      topic: topic,
-      date: threeMonths,
-    },
-    {
-      topic: topic,
-      date: sixMonths,
-    },
-    {
-      topic: topic,
-      date: oneYear,
-    },
-  ];
-//}
 
 function addUsersToDropdown() {
   for (const user of users) {
