08e1986b095a5891622f912093df92819c32c77e
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-06-11 18:28:32 +0100

    add tests for createAgendaItem function to verify revision dates

---
+++ agenda.test.mjs
@@ -0,0 +1,16 @@
+import test from "node:test";
+import assert from "node:assert";
+import { createAgendaItem } from "./agenda.mjs";
+test("create revision dates from a start date",()=>{
+    const topic="Practice arrays"
+    const startDate="2027-07-19"
+    const result=createAgendaItem(topic,startDate)
+    
+    assert.equal(result.length, 5);
+    assert.equal(result[0].date.toISOString().split("T")[0], "2027-07-26");
+    assert.equal(result[1].date.toISOString().split("T")[0], "2027-08-19");
+    assert.equal(result[2].date.toISOString().split("T")[0], "2027-10-19");
+    assert.equal(result[3].date.toISOString().split("T")[0], "2028-01-19");
+    assert.equal(result[4].date.toISOString().split("T")[0], "2028-07-19");
+    
+})
